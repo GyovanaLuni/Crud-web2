9310c877ee8e4d0e87a1290cfaf8d87db210035f
Camila Silva <silva.camilam27@gmail.com>
2022-01-12 22:40:16 -0300

    ajustando códigos css

--- projetoweb2/settings.py
+++ projetoweb2/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
